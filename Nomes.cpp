fd63d337099d7e9668b336c76d94dddcc480928f
TiagoVittorio <156953427+TiagoVittorio@users.noreply.github.com>
2024-11-07 14:22:56 +0000

    Nomes.cpp

--- Nomes.cpp
+++ Nomes.cpp
@@ -7,38 +7,23 @@ int main() {
     setlocale(LC_ALL, "Portuguese");
 
     int i;
-    char escolher;
+    char  escolher = 's';
     string nomes[5];
 
- 
-    for (i = 0; i < 5; i++) {
-        cout << "Digite o " << i + 1 << "º nome: ";
-        cin >> nomes[i];
-    }
+    do {
+        for (i = 0; i < 5; i++) {
+            cout << "Digite o " << i + 1 << "º nome: ";
+            cin >> nomes[i];
+        }
 
 
-    cout << "\nNomes na ordem de trás para a frente:\n";
-    for (i = 4; i >= 0; i--) {
-        cout << nomes[i];
-    }
 
-    cout << "\nDeseja sair? (s para sair / n para continuar): ";
-    cin >> escolher;
+        for (i = 4; i >= 0; i--) {
+            cout << nomes[i];
+        }
 
-    switch (escolher) {
-    case 's':
-    case 'S':
-        cout << "A fechar  o programa.\n";
-        break;
-    case 'n':
-    case 'N':
-        cout << "Continuar o programa...\n";
-       
-        break;
-    default:
-        cout << "Opção errada, por favor escolhe 's' ou 'n'!\n";
-        break;
-    }
+        cout << "\nDeseja sair? (s para sair / n para continuar): ";
+        cin >> escolher;
+    } while (escolher == 'n');
 
-    return 0;
 }
